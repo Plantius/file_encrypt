a7a4b793e603ea9916e30031aa4194a4dc1689b6
niels <niels>
2024-06-23 22:03:02 +0200

    Changed hash

--- src/encrypt.cc
+++ src/encrypt.cc
@@ -12,7 +12,7 @@ password_encrypt(const std::string pwd, const char* buffer, const size_t size)
 
     outbuffer[0] = uint8_t(buffer[0] + hash_function(pwd)) % SHIFT;   
     for (size_t i = 1; i < size; i++)
-        outbuffer[i] = int(outbuffer[i-1] + buffer[i] + hash_function(pwd)) % SHIFT;   
+        outbuffer[i] = uint8_t(outbuffer[i-1] + buffer[i] + hash_function(pwd)) % SHIFT;   
     
     return outbuffer;
 }
@@ -28,7 +28,7 @@ password_decrypt(const std::string pwd, const char* buffer, const size_t size)
 
     outbuffer[0] = uint8_t(buffer[0] - hash_function(pwd)) % SHIFT;   
     for (size_t i = 1; i < size; i++)
-        outbuffer[i] = int(buffer[i] - buffer[i-1] - hash_function(pwd) + SHIFT) % SHIFT;   
+        outbuffer[i] = uint8_t(buffer[i] - buffer[i-1] - hash_function(pwd) + SHIFT) % SHIFT;   
     
     return outbuffer;
 }
@@ -40,5 +40,5 @@ hash_function(const std::string pwd)
     for (size_t i = 0; i < pwd.size(); i++)
         hash += int(pwd[i]);
     
-    return (hash * secret) % (1 << 8);
+    return (hash * sec2) % sec1;
 }
--- src/standard.h
+++ src/standard.h
@@ -8,7 +8,7 @@
 
 const size_t MAX_OPT = 3;
 const int MAX_PWD = 256;
-const uint32_t secret = 947;
+const uint32_t sec1 = 947, sec2 = 115289591;
 const int SHIFT = 1 << 8;
 
 std::string get_pwd(); 
